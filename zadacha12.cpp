89655528268453b21a368d7177d517238e173bcd
ZDRAVKO107UKTC <146438118+ZDRAVKO107UKTC@users.noreply.github.com>
2026-01-12 10:35:40 +0200

    second test function

--- zadacha12.cpp
+++ zadacha12.cpp
@@ -9,8 +9,25 @@ double safe_divide(double a, double b) {
         }
         return a / b;
 };
+void dynamic_array_test(){
+    int size;
+    cout << "Enter size of array: ";
+    cin >> size;
+    int* dynamic_arr = new int[size];
+    for (int i = 0; i < size; i++) {
+        dynamic_arr[i] = i;
+    }
+    cout << "Array elements are: ";
+    for (int i = 0; i < size; i++) {
+        cout << dynamic_arr[i] << " ";
+    }
+    delete[] dynamic_arr;
+}
 
 int main() {
+    dynamic_array_test();
+
+    cout <<"\n================="<< endl;
     try {
         cout << safe_divide(10, 2) << endl;
         cout << safe_divide(10, 0) << endl;
